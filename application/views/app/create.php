7dbc8d3c37e6f9e24afd3e63b971aa2fdf85a538
Galih Pratama <hanamura.iost@gmail.com>
2013-11-03 02:07:49 +0700

    change create notes ui

--- application/views/app/create.php
+++ application/views/app/create.php
@@ -36,15 +36,58 @@ tinymce.init({
 <!-- /TinyMCE -->
 
 <div class="container notebook-container">
+<div class="pull-right menu-button">
+  <a class="btn btn-primary btn-sm">
+    <i class="fa fa-save icon-white"></i> Simpan
+  </a>
+  <a class="btn btn-danger btn-sm" href="javascript:history.back()">
+    <i class="fa fa-trash-o icon-white"></i>
+  </a>
+</div>
+<div class="pull-right menu-button">
+  <div class="bfh-selectbox">
+  <input type="hidden" name="selectbox3" value="12">
+  <a class="bfh-selectbox-toggle" role="button" data-toggle="bfh-selectbox" href="#">
+    <span class="bfh-selectbox-option bfh-selectbox-medium" data-option="12">&nbsp; <i class="fa fa-book"></i> Pilih Jurnal</span>
+    <b class="caret"></b>
+  </a>
+  <div class="bfh-selectbox-options">
+    <input type="text" class="bfh-selectbox-filter">
+    <div role="listbox">
+      <ul role="option">
+        <li><a tabindex="-1" href="#" data-option="1">&nbsp; <i class="fa fa-book"></i> Personal</a></li>
+        <li><a tabindex="-1" href="#" data-option="2">&nbsp; <i class="fa fa-book"></i> Work</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+</div>
+<div class="pull-right menu-button">
+  <div class="bfh-selectbox">
+  <input type="hidden" name="selectbox3" value="12">
+  <a class="bfh-selectbox-toggle" role="button" data-toggle="bfh-selectbox" href="#">
+    <span class="bfh-selectbox-option" data-option="12">&nbsp; <i class="fa fa-stop"></i> Hitam &nbsp; </span>
+    <b class="caret"></b>
+  </a>
+  <div class="bfh-selectbox-options">
+    <div role="listbox">
+      <ul role="option">
+        <li><a tabindex="-1" href="#" data-option="1">&nbsp; <i class="fa fa-stop text-blue"></i> Biru &nbsp; </a></li>
+        <li><a tabindex="-1" href="#" data-option="2">&nbsp; <i class="fa fa-stop text-brown"></i> Coklat &nbsp; </a></li>
+        <li><a tabindex="-1" href="#" data-option="2">&nbsp; <i class="fa fa-stop text-red"></i> Merah &nbsp; </a></li>
+        <li><a tabindex="-1" href="#" data-option="2">&nbsp; <i class="fa fa-stop text-pink"></i> Pink &nbsp; </a></li>
+        <li><a tabindex="-1" href="#" data-option="2">&nbsp; <i class="fa fa-stop text-green"></i> Hijau &nbsp; </a></li>
+        <li><a tabindex="-1" href="#" data-option="2">&nbsp; <i class="fa fa-stop text-black"></i> Hitam &nbsp; </a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+</div>
+
 <h1>
   <i class="fa fa-book"></i> <input class="edit-header" type="text" placeholder="Judul Catatan...">
-
-  <span class="pull-right">
-    <a class="btn btn-primary btn-small">
-      <i class="fa fa-save icon-white"></i> Simpan
-    </a>
-  </span>
 </h1>
+
 <hr>
     <textarea name="edit" height="30"></textarea>
 </div>
