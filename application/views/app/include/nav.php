404e74c0ed1c97d0ba036d2b50bb6abed553b3c8
Galih Pratama <hanamura.iost@gmail.com>
2013-11-03 20:16:24 +0700

    fix nav

--- application/views/app/include/nav.php
+++ application/views/app/include/nav.php
@@ -11,20 +11,18 @@ if ($this->uri->segment(2) != 'login') {
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Jurnalme</a>
+      <a class="navbar-brand" href="<?php echo base_url();?>app/"><img src="<?php echo base_url(); ?>/assets/img/jurnalme-logo.png" style="height: 23px;" alt=""></a>
     </div>
     <div class="collapse navbar-collapse">
       <ul class="nav navbar-nav">
-        <li><a href="<?php echo base_url();?>app/">Semua Catatan</a></li>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown"> Jurnal <b class="caret"></b></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown"> <i class="fa fa-book"></i> Jurnal Saya</a>
           <ul class="dropdown-menu" role="menu">
             <li><a href="#"><i class="fa fa-book"></i> Personal</a></li>
             <li><a href="#"><i class="fa fa-book"></i> Work</a></li>
-            <li class="divider"></li>
-            <li><a href="#"><i class="fa fa-plus"></i> Tambah Jurnal</a></li>
           </ul>
         </li>
+        <li><a href="#" data-toggle="modal" data-target="#buatjurnal"><i class="fa fa-plus-circle"></i> Tambah Jurnal</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li class="dropdown">
