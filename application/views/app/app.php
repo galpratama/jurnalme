aa1dc4ff560db69787e1417b2da50576d704373d
Galih Pratama <hanamura.iost@gmail.com>
2013-11-12 19:06:27 +0700

    fix indentation and remove dummy comtent

--- application/views/app/app.php
+++ application/views/app/app.php
@@ -1,158 +1,36 @@
 <div class="container notebook-container">
-<h1>	
-  <i class="fa fa-book"></i> 
-  <span id="judul-jurnal"><b>Semua Catatan</b>
-  <a href="" id="edit-judul-jurnal" class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></a>
-  <a href="" id="hapus-judul-jurnal" class="btn btn-danger btn-xs"><i class="fa fa-trash-o"></i></a>
-  </span>
-  <span class="pull-right">
-    <a class="btn btn-warning btn-small" href="<?php echo base_url();?>app/create">
-      <i class="fa fa-plus icon-white"></i> Buat Catatan Baru
-    </a>
-  </span>
-</h1>
+  <h1>	
+    <i class="fa fa-book"></i> 
+      <span id="judul-jurnal"><b>Semua Catatan</b>
+        <a href="" id="edit-judul-jurnal" class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></a>
+        <a href="" id="hapus-judul-jurnal" class="btn btn-danger btn-xs"><i class="fa fa-trash-o"></i></a>
+      </span>
+      <span class="pull-right">
+        <a class="btn btn-warning btn-small" href="<?php echo base_url();?>app/create">
+          <i class="fa fa-plus icon-white"></i> Buat Catatan Baru
+      </a>
+      </span>
+  </h1>
 <hr>
-    <div id="pin-outer">
-
+<div id="pin-outer">
+  <?php
+    foreach ($notes->result() as $notes)
+    {
+      ?>
         <div class="col-lg-4 pin note-bungkus pin-inner">
-           <a href="/"><h2>HTML5 Startup Engineering</h2></a>
-              <p class="post-snippet">
-                  <b><i class="fa fa-clock-o"></i> 17 September 2013</b> &middot; <i class="fa fa-book"></i> <b>Personal</b>
-              </p>
-          <br>
-          <div class="note-bungkus notes notes-blue">
-            <p><strong>Tutorial Git</strong><br /> - Pasang git-scm cari di google<br /> - ke direktori project<br /> - tulis dibawah kayak gini<br /> <em>&gt; git init</em><br /> <em>&gt; git add README.md</em><br /> <em>&gt; git commit -m "first commit"</em><br /> - kalau mau nambahin branch ke github<br /> <em>&gt; git remote add origin https://github.com/galpratama/booklog-camp.git</em><br /> <em>&gt; git push -u origin master</em></p>
-			<p><strong>Kebutuhan sebelum install Jade :</strong><br /> - Install Node.js&nbsp;<span style="text-decoration: underline;">http://nodejs.org/</span><br /> - Tulis di terminal : npm install --global jade</p>
-			<p><strong>Dokumentasi Jade</strong><br /> <span style="text-decoration: underline;">http://jade-lang.com/</span></p>
-          </div>
-
-          
-
-        </div>
-
-        <div class="col-lg-4 pin note-bungkus pin-inner">
-           
-              <a href="/"><h2>Agar Menjadi Orang Sukses</h2></a>
-              <p class="post-snippet">
-                  <b><i class="fa fa-clock-o"></i> 17 September 2013</b> &middot; <i class="fa fa-book"></i> <b>Personal</b>
-              </p>
-          
-          <br>
-           <div class="note-bungkus notes notes-red">
-         
-              <ol>
-					<li><strong>Olahraga</strong><br />Olahraga pagi itu penting. Lakukan baik di&nbsp;<em>gym</em>&nbsp;atau bentuk olahraga lain. Olahraga pagi selain penting untuk persiapan kerja sepanjang hari juga memperkecil kemungkinan tertimbunnya flek pada jantung.</li>
-					<li><strong>Petakan rencana kerja hari ini.<br /></strong>Ini dilakukan untuk mengoptimalkan potensi yang dimiliki. Dengan membuat jadwal yang akan dikerjakan sepanjang hari ini semua pekerjaan akan bisa dilakukan dengan baik sesuai prioritas. Kebiasaan ini seperti sarapan. Jika sarapan pagi kita baik maka sepanjang hari akan baik. Jika persiapan kita baik maka pekerjaan kita pun akan baik.</li>
-					<li><strong>Sarapan pagi yang sehat.<br />&nbsp;</strong>Sudah banyak yang menganjurkan sebelum bekerja sebaiknya sarapan. Banyak yang melakukan sarapan pagi di kantor karena kesibukan atau terburu-buru karena bangun agak siang. Tetapi akan lebih baik jika itu dilakukan di rumah. Selain lebih sehat juga tetap menjaga kedekatan dengan keluarga.</li>
-					<li><strong>Visualisasi.<br /></strong>Bayangkan sukses apa yang akan diraih hari ini. Membuat visualisasi keberhasilan yang mau diraih hari ini akan membantu menjaga&nbsp;<em>mood</em>&nbsp;sepanjang hari. Meski hanya memvisualisasikannya barang semenit, itu akan berpengaruh.</li>
-					<li><strong>Jadikan hari Anda padat.</strong> <br />Setiap dari kita pasti punya daftar pekerjaan yang menakutkan. Kadang selalu ditunda untuk melakukannya sehingga akhirnya banyak waktu terbuang percuma dan membuat stres. Untuk mengatasinya coba masukkan pekerjaan terberat itu untuk dilakukan sepagi mungkin atau setidaknya menjelang makan siang. Hal ini penting karena di pagi hari kondisi tubuh lebih segar dan tenaga sedang penuh. Dengan demikian masalah berat itu akan lebih mudah diatasi.&nbsp;</li>
-				</ol>
-           </div>
-
-        </div>
-        <div class="col-lg-4 pin note-bungkus pin-inner">
-           
-          <a href="/"><h2>Virtual Company</h2></a>
+          <a href="/"><h2><?php echo $notes->notes_title;?></h2></a>
           <p class="post-snippet">
-              <b><i class="fa fa-clock-o"></i> 12 September 2013</b> &middot; <i class="fa fa-book"></i> <b>Work</b>
+            <b><i class="fa fa-clock-o"></i> <?php echo $notes->notes_date;?></b> &middot; <i class="fa fa-book"></i> <b><?php echo $notes->notes_notebooks_id;?></b>
           </p>
-          
           <br>
-           <div class="note-bungkus notes notes-brown">
-              <ol>
-				<li>Company Profile
-					<ul>
-						<li>Nama Perusahaan + Logo</li>
-						<li>Latar Belakang</li>
-						<li>Portfolio</li>
-						<li>What to Offer</li>
-					</ul>
-				</li>
-				<li>Business Model</li>
-				<li>Individual Profile</li>
-				<li>Analisis SWOT</li>
-				<li>Apa yang kamu harapkan?&nbsp;</li>
-				</ol>
-                        
-           </div>
-
-
+          <div class="note-bungkus notes notes-<?php echo $notes->notes_color;?>">
+            <p>
+              <?php echo $notes->notes_content;?>
+            </p>
+          </div>
         </div>
-        <div class="col-lg-4 pin note-bungkus pin-inner">
-           <a href="/"><h2>Tugas Perkuliahan 22</h2></a>
-              <p class="post-snippet">
-                  <b><i class="fa fa-clock-o"></i> 1 Oktober 2013</b> &middot; <i class="fa fa-book"></i> <b>Personal</b>
-              </p>
-          <br>
-           <div class="note-bungkus notes notes-white">
-              	<p><strong>Video Dokumentasi</strong><br /> Buatlah sebuah video dokumentasi berupa cara pembuatan aplikasi sederhana yang menggunakan MySQL, misalnya aplikasi daftar biodata mahasiswa, aplikasi inventarisasi peralatan dll.<br /> Upload video tersebut ke Youtube<br /> Kumpulkan pada instruktur USBJJ</p>
-				<p><strong>Laporan Aktivitas Belajar</strong><br /> Kumpulkan pada instruktur USBJJ</p>
-				<p><strong>Portofolio Blog</strong><br /> Lakukan posting berupa memindahkan (copy-paste) isi Laporan pada blog.<br /> Tuliskan link halaman blog tersebut di sini.&nbsp;</p>
-           </div>
-
-        </div>
-        <div class="col-lg-4 pin note-bungkus pin-inner">
-           <a href="/"><h2>DayaHosting</h2></a>
-              <p class="post-snippet">
-                  <b><i class="fa fa-clock-o"></i> 1 Oktober 2013</b> &middot; <i class="fa fa-book"></i> <b>Work</b>
-              </p>
-          <br>
-           <div class="note-bungkus notes notes-green">
-				<ul style="list-style-type: square;">
-					<li><span style="text-decoration: line-through;">Beresin paket Hosting yang mau dijual nanti</span></li>
-					<li><span style="text-decoration: line-through;">Pesen Master Reseller Hosting</span></li>
-					<li>Migrasi semua akun dari PesanHost ke LebahNetwork</li>
-					<li>Website LebahNetwork</li>
-					<li>Kontak Ardhosting buat domain</li>
-					<li>WHMCS</li>
-					<li>Client Area : Customer Support</li>
-				</ul>
-           </div>
-
-        </div>
-        <div class="col-lg-4 pin note-bungkus pin-inner">
-           <a href="/">
-              <h2>Lorem Ipsum</h2>
-              <p class="post-snippet">
-                  This is a post about title one. 
-              </p>
-          </a>
-          <br>
-           <div class="note-bungkus notes notes-black">
-              <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
-           </div>
-
-        </div>
-        <div class="col-lg-4 pin note-bungkus pin-inner">
-           <a href="/">
-              <h2>Lorem Ipsum</h2>
-              <p class="post-snippet">
-                  This is a post about title one. 
-              </p>
-          </a>
-          <br>
-           <div class="note-bungkus notes notes-pink">
-              <p>Includes 200 glyphs in font format from the Glyphicon Halflings set. <a href="http://glyphicons.com/">Glyphicons</a> Halflings are normally not available for free, but their creator has made them available for Bootstrap free of cost. As a thank you, we only ask that you to include a link back to <a href="http://glyphicons.com/">Glyphicons</a> whenever possible.</p>  
-            </div>
-
-        </div>
-        <div class="col-lg-4 pin note-bungkus pin-inner">
-           <a href="/">
-              <h2>Daftar Belanjaan</h2>
-              <p class="post-snippet">
-                  This is a post about title one. 
-              </p>
-          </a>
-          <br>
-           <div class="note-bungkus notes notes-white">
-              <ol>
-                 <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-                 <li>Aliquam tincidunt mauris eu risus.</li>
-                 <li>Vestibulum auctor dapibus neque.</li>
-              </ol>
-           </div>
-
-        </div>
-
-    </div>
+      <?php
+    }
+  ?>
+  
 </div>
