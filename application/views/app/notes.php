bbe71f4cd725802dd917aa8123fe5bb1ae7baf66
Galih Pratama <hanamura.iost@gmail.com>
2013-11-14 20:13:45 +0700

    fix link

--- application/views/app/notes.php
+++ application/views/app/notes.php
@@ -10,7 +10,7 @@
           <i class="fa fa-chevron-left icon-white"></i> Kembali
         </a>
         <a href="<?php echo base_url();?>app/edit/<?php echo $notes->notes_id;?>" class="btn btn-primary btn-small"><i class="fa fa-pencil"></i> Edit</a>
-        <a href="#" data-toggle="modal" data-target="#deletenotebooks" class="btn btn-danger btn-small"><i class="fa fa-trash-o"></i> Hapus</a>
+        <a href="#" data-toggle="modal" data-target="#deletenotes" class="btn btn-danger btn-small"><i class="fa fa-trash-o"></i> Hapus</a>
         
       </span>
   </h2>
@@ -24,5 +24,18 @@
     <p>
       <?php echo $notes->notes_content;?>
     </p>
+  </div>
+</div>
+
+<div class="modal fade" id="deletenotes" role="dialog" aria-labelledby="notesLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-body">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h3 class="modal-title text-center" id="myModalLabel"><b><?php echo $notes->notes_title;?></b></h3>
+        <p class="text-center">Apakah kamu yakin ingin menghapus <b></b> catatan ini? <br><strong class="text-danger">Aksi ini tidak bisa dibatalkan!</strong></p>
+        <a href="<?php echo base_url();?>app/notes_delete/<?php echo $notes->notes_id;?>" class="btn btn-danger btn-block btn-lg" data-dismiss="modal">Hapus</a>
+      </div>
+   </div>
   </div>
 </div>
